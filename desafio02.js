bca25db465afea1f129c72558bdd2baf770934f9
Paulo <paulo2507@hotmail.com>
2023-11-22 16:42:49 -0300

    Desafio 02 web dio

---
+++ desafio02.js
@@ -0,0 +1,31 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+  
+    if (saldoVitorias < 10) {
+        nivel = "Ferro";
+    } else if (saldoVitorias >= 10 && saldoVitorias <= 20) {
+        nivel = "Bronze";
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = "Prata";
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = "Ouro";
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = "Diamante";
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+  
+    //console.log ('O Herói tem saldo de ' + saldoVitorias + ' e está no nível '+ nivel)//
+    
+    const mensagem = `O Herói tem saldo de ${saldoVitorias} está no nível de ${nivel}`;
+    console.log(mensagem);
+  }
+  
+  // Exemplo de uso
+  const vitorias = 100;
+  const derrotas = 5;
+  calcularNivel(vitorias, derrotas);
+  
